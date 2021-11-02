8fdb23bb6765187d9ca2bc1f3023c01bd0681db3
darlanMedeiros <darlan.smedeiros@gmail.com>
2021-11-02 00:26:37 -0300

    Static charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,28 @@
+import BarChart from "components/BarChart";
 import DataTable from "components/DataTable";
+import DonutChart from "components/DonutChart";
 import Footer from "components/Footer";
 import NavBar from "components/NavBar";
 
 function App() {
   return (
     <>
-    <NavBar />
-    <div className = "container">    
-
-      <h1 className="text-primary">Olá Mundo</h1>
-
-      <DataTable />     
-      
-    </div>
-    <Footer />
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary py-3">Dashoard de Vendas</h1>
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5> Taxa de sucesso (%) </h5>
+            <BarChart />
+          </div>
+          <div className="col-sm-6">
+            <h5> Todas vendas </h5>
+            <DonutChart/>
+          </div>
+        </div>
+        <DataTable />
+      </div>
+      <Footer />
     </>
   );
 }
